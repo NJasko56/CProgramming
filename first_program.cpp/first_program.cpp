c3b17fb6cfc418610588099ac6834bce31ffb74f
NJasko56 <njasko0784@floridapoly.edu>
2026-02-13 14:40:59 +0000

    Finally, saved changes

---
+++ first_program.cpp/first_program.cpp
@@ -0,0 +1,19 @@
+/*
+    The first C++ program.
+    01/14/2006
+*/
+
+#include <iostream>
+
+int main(void) {
+    std::cout << "Hello from COP3337! \n" << std::endl;
+
+    std::cout << "(15* 78) + 14 is" << (15* 78) + 14 <<std::endl;
+    
+    //std - standard namespace
+    //cout - console output (output of iostream class)
+    //<< - stream insertion operator
+    //endl - endl (move the cursor in the terminal)
+
+    return 0;
+}
